07a08e13d249ea09ce9f275d48202fe6b18057e8
Oliver Lockwood (CBRE) <v-ollock@microsoft.com>
2020-04-30 15:35:07 -0700

    z

---
+++ students/scotchwsplenda/Lesson_9/fuckaround.py
@@ -0,0 +1,20 @@
+# class Dog():
+#
+#     dogs = []
+#
+#     def __init__(self, age, name):
+#         self.age = age
+#         self.name = name
+#         self.dogs.append([self])
+#
+#
+# Dog(22, 'buggy')
+# Dog(33, 'ted')
+#
+# print(Dog.dogs)
+#
+# for x in Dog.dogs:
+#     print(x)
+
+for x in range(10):
+    print(x)
